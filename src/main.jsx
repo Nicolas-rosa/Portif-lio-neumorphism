401ccaf649c8fd0deb817980c19a2d6248797829
Nicolas Rosa <nicolasfrancacastrorosa@gmail.com>
2024-12-03 16:16:12 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Header from './Components/Header.jsx'
-import Techs from './Components/Techs.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header/>
-    <Techs/>
+  
     <App />
   </StrictMode>,
 )
