6be25cd5b96084abc83035700e15ad86b972e9db
Nicolas Rosa <nicolasfrancacastrorosa@gmail.com>
2024-12-03 16:16:46 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import React from "react"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Curriculo from "./pages/Curriculo"
-import Portifolio from "./pages/Portifolio"
-import Home from "./pages/Home"
-import Contato from "./pages/Contato"
-import './index.css'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Curriculo from "./pages/Curriculo";
+import Portifolio from "./pages/Portifolio";
+import Home from "./pages/Home";
+import Contato from "./pages/Contato";
+import './index.css';
+import Header from './Components/Header.jsx'
+import Techs from './Components/Techs.jsx'
 
 export default function App() {
   return (
-<BrowserRouter>
-<Routes>
-<Route Component = { Curriculo } path="/Curriculo" />
-<Route Component = { Portifolio } path="/portifolio" />
-<Route Component={ Home } path="/" />
-<Route Component={ Contato } path="/contato" />
-</Routes>
-
-</BrowserRouter>
-  )
+    <BrowserRouter>
+      <Header/>
+      <Techs/>
+      <Routes>
+        <Route element={<Curriculo />} path="/curriculo" />
+        <Route element={<Portifolio />} path="/portifolio" />
+        <Route element={<Home />} path="/" />
+        <Route element={<Contato />} path="/contato" />
+      </Routes>
+    </BrowserRouter>
+  );
 }
